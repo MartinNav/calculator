eab6a59f4e008293e86ff1243295e881a9699ca3
MartinNav <martinnavratilu@gmail.com>
2024-03-10 18:31:00 +0100

    Added documentation and tests

--- src/math_lib/src/execute.rs
+++ src/math_lib/src/execute.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 
+/// [EvaluationError] indicates malformation in parse tree
 #[derive(Debug, Clone, PartialEq)]
 pub struct EvaluationError;
 impl fmt::Display for EvaluationError {
@@ -7,7 +8,9 @@ impl fmt::Display for EvaluationError {
         write!(f, "Error while evaluating parse tree occurred")
     }
 }
-
+/// This function is doing the actual math calculations on the parse tree
+/// When the parse tree is not valid or is malformed it will return [EvaluationError].
+/// In numerical edge cases such as division by zero `inf` or `NaN` will be returned for more information please visit [f64] documentation 
 pub fn evaluate_parse_tree(parse_tree: String)->Result<f64, EvaluationError>
 {
     Err(EvaluationError)
@@ -20,6 +23,40 @@ mod tests{
     #[test]
     fn add_two_values() {
         assert_eq!(evaluate_parse_tree("2 2 +".to_string()), Ok(4.0));
+    }
+    #[test]
+    fn subtract_two_values() {
+        assert_eq!(evaluate_parse_tree("2 2 -".to_string()), Ok(0.0));
+    }
+    #[test]
+    fn multiply_two_values() {
+        assert_eq!(evaluate_parse_tree("2 4 *".to_string()), Ok(8.0));
+    }
+    #[test]
+    fn divide_two_values() {
+        assert_eq!(evaluate_parse_tree("8 2 /".to_string()), Ok(4.0));
+    }
+
+    #[test]
+    fn composed_equation_1() {
+        assert_eq!(evaluate_parse_tree("3 2 1 + *".to_string()), Ok(6.0));
+    }
+
+    #[test]
+    fn composed_equation_2() {
+        assert_eq!(evaluate_parse_tree("3 4 * 2 5 * +".to_string()), Ok(22.0));
+    }
+
+
+
+    //These are invalid operations
+    #[test]
+    fn invalid_multiple_ops() {
+        assert_eq!(evaluate_parse_tree("2 2 + * -".to_string()), Err(EvaluationError));
+    }
+    #[test]
+    fn invalid_character() {
+        assert_eq!(evaluate_parse_tree("2 2 $".to_string()), Err(EvaluationError));
     }
 
 }
--- src/math_lib/src/lib.rs
+++ src/math_lib/src/lib.rs
@@ -1,1 +1,2 @@
 mod execute;
+pub use crate::execute::*;
