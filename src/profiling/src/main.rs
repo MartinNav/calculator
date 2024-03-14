b797ba9bb95e4ba8c04d56f54c41b832af9504c9
Terochi <mates.cernicky@gmail.com>
2024-03-14 21:42:41 +0100

    Added docs

--- src/profiling/src/main.rs
+++ src/profiling/src/main.rs
@@ -1,7 +1,6 @@
-mod tests;
-
 use std::io::{Read, stdin};
 
+/// Takes values for a standard deviation calculation and processes it to a string that can used by the parser.
 pub fn prepare_to_parse(input: String) -> String {
     let numbers = input
         .split_whitespace()
@@ -14,17 +13,23 @@ pub fn prepare_to_parse(input: String) -> String {
     let total_length = 16 + 4 * numbers.len() + 2 * numbers_total_char_length + number_count.len() + number_count_minus_one.len();
     let mut expression = String::with_capacity(total_length);
 
+    // s
     expression.push_str("R(");
+    // 1/(N-1)
     expression.push_str("1/");
     expression.push_str(number_count_minus_one.as_str());
+    // (Σxi^2 - 1/N * (xt^2))
     expression.push_str("*(");
+    // Σxi^2
     for number in &numbers {
         expression.push_str(number);
         expression.push_str("^2+");
     }
     expression.pop();
+    // -1/N * (xt^2)
     expression.push_str("-1/");
     expression.push_str(number_count.as_str());
+    // xt = Σxi
     expression.push_str("*(");
     for number in &numbers {
         expression.push_str(number);
