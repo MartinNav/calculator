26941b5cf4ddf754c173b2726612a0844ff4acd2
Terochi <mates.cernicky@gmail.com>
2024-04-21 18:49:13 +0200

    Added docs

--- src/profiling/src/main.rs
+++ src/profiling/src/main.rs
@@ -1,7 +1,15 @@
 use std::io::{Read, stdin};
 use math_lib::parse;
 
-/// Takes values for a standard deviation calculation and processes it to a string that can used by the parser.
+/// Prepares a string to be used by the parser to calculate the standard deviation.
+///
+/// # Arguments
+///
+/// * `input` - A string of numbers separated by whitespace.
+///
+/// # Returns
+///
+/// * A string that can be used by the parser.
 pub fn prepare_to_parse(input: String) -> String {
     let numbers = input
         .split_whitespace()
@@ -14,28 +22,30 @@ pub fn prepare_to_parse(input: String) -> String {
     let total_length = 17 + 4 * numbers.len() + 2 * numbers_total_char_length + number_count.len() + number_count_minus_one.len();
     let mut expression = String::with_capacity(total_length);
 
+    // 2√(
     expression.push_str("2√(");
-    // 1/(N-1)
+    // 2√(1/(N-1)
     expression.push_str("1/");
     expression.push_str(number_count_minus_one.as_str());
-    // (Σxi^2 - 1/N * (xt^2))
+    // 2√(1/(N-1)*(
     expression.push_str("*(");
-    // Σxi^2
+    // 2√(1/(N-1)*(Σxi^2)
     for number in &numbers {
         expression.push_str(number);
         expression.push_str("^2+");
     }
     expression.pop();
-    // -1/N * (xt^2)
+    // 2√(1/(N-1)*(Σxi^2)-1/N
     expression.push_str("-1/");
     expression.push_str(number_count.as_str());
-    // xt = Σxi
+    // 2√(1/(N-1)*(Σxi^2)-1/N*(Σxi)^2
     expression.push_str("*(");
     for number in &numbers {
         expression.push_str(number);
         expression.push('+');
     }
     expression.pop();
+    // 2√(1/(N-1)*(Σxi^2)-1/N*((Σxi)^2)))
     expression.push_str(")^2))");
 
     assert_eq!(expression.len(), total_length);
